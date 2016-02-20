1d4700ac63727514a3bb9d26b3d500445437dbfe
unknown <Andrei_Stanisheuski1@epam.com>
2016-02-20 21:54:55 +0300

    Adding actions for mobile devices(navbar-links)

--- js/my_js.js
+++ js/my_js.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
 	var sidebar = '.sidebar';
-	var sectionAbout = '.about';
-	var sectionSome = '.some';
 	var navbarLinks = '.navbar > a';
 	var nav = '.nav';
-	var linkAbout = 'link-about';
-	var linkSome = 'link-some';
-	var linkTabs = '.link-tabs';
 	var linkNav = '.link-nav';
 	var display = 'display';
-	var hide = 'hide';
-	var tags = 'tags';
 	var sidebarContainer = $(sidebar).children();
 
 	$(navbarLinks).on('click', function() {
+		$(nav).hide();
 		var linkClass = $(this).attr('class');
 		$(sidebar).show();
 		sidebarContainer.hide();
 		sidebarContainer.filter('.' + linkClass).show();
+	});
+
+	$(linkNav).on('click', function() {
+		$(sidebar).hide();
+		$(nav).css('display', 'flex');
 	});
 });
