da0058d4bcf0f5d5f55132c22b85730b2efca64d
unknown <Andrei_Stanisheuski1@epam.com>
2016-02-20 19:41:11 +0300

    Adding actions for links from nav(without link-nav) for mobile resolution

--- js/my_js.js
+++ js/my_js.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-	// var aboutNavLink = ''
-	// var someNavLink =
-	// var tabsNavLink =
-	// var nav =
-	// var navigationLink =
+	var sidebar = '.sidebar';
+	var sectionAbout = '.about';
+	var sectionSome = '.some';
+	var navbarLinks = '.navbar > a';
+	var nav = '.nav';
+	var linkAbout = 'link-about';
+	var linkSome = 'link-some';
+	var linkTabs = '.link-tabs';
+	var linkNav = '.link-nav';
+	var display = 'display';
+	var hide = 'hide';
+	var tags = 'tags';
+	var sidebarContainer = $(sidebar).children();
+
+	$(navbarLinks).on('click', function() {
+		var linkClass = $(this).attr('class');
+		$(sidebar).show();
+		sidebarContainer.hide();
+		sidebarContainer.filter('.' + linkClass).show();
+	});
 });
