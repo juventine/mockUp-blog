28e8a7d1597f6aefe50da508a4b3e2438583d787
unknown <Andrei_Stanisheuski1@epam.com>
2016-02-21 18:05:36 +0300

    Adding folding for the sidebar(mobile devices)

--- js/my_js.js
+++ js/my_js.js
@@ -4,18 +4,31 @@ $(document).ready(function() {
 	var nav = '.nav';
 	var linkNav = '.link-nav';
 	var display = 'display';
+	var activeTab = 'active-tab';
+	var sidebarTitle = '.title';
+	var foldIcon = '.fold-icon';
+
 	var sidebarContainer = $(sidebar).children();
 
 	$(navbarLinks).on('click', function() {
 		$(nav).hide();
 		var linkClass = $(this).attr('class');
+		$(linkClass).addClass(activeTab);
 		$(sidebar).show();
 		sidebarContainer.hide();
 		sidebarContainer.filter('.' + linkClass).show();
 	});
 
 	$(linkNav).on('click', function() {
-		$(sidebar).hide();
+		hideSidebar();
 		$(nav).css('display', 'flex');
 	});
+
+	$(sidebarTitle).on('click', hideSidebar);
+	$(foldIcon).on('click', hideSidebar);
+
+	function hideSidebar() {
+		$(sidebar).hide();
+	}
+
 });
