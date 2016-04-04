a709c481e1d0efbc9ed5851304f721f108126ffc
unknown <Andrei_Stanisheuski1@epam.com>
2016-04-04 12:14:58 +0300

    Refactoring work: semantics

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,38 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
+	grunt.initConfig({
 
-    less: {
-      development: {
-        files: {
-            "css/styles.css": "less/styles.less"
-          }
-      },
-    },
+		less: {
+			development: {
+				files: {
+						"css/styles.css": "less/styles.less"
+					}
+			},
+		},
 
-    watch: {
-      css: {
-        files: ['less/*.less'],
-        tasks: ['compileLess'],
-        options: {
-          spawn: false,
-        }
-      }
-    },
+		watch: {
+			css: {
+				files: ['less/*.less'],
+				tasks: ['compileLess'],
+				options: {
+					spawn: false,
+				}
+			}
+		},
 
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 versions', 'ie 8', 'ie 9']
-      },
-      no_dest: {
-        src: "css/styles.css"
-      }
-    }
-  });
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie 8', 'ie 9']
+			},
+			no_dest: {
+				src: "css/styles.css"
+			}
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 
-  grunt.task.registerTask('compileLess', ['less','autoprefixer']);
-  grunt.task.registerTask('dev', ['compileLess','watch']);
-
+	grunt.task.registerTask('compileLess', ['less','autoprefixer']);
+	grunt.task.registerTask('dev', ['compileLess','watch']);
 };
